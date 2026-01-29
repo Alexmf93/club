d5136b70c2e19dfc3856e1e2d4598eba97e9c847
Alexmf93 <alex_moya_12@hotmail.com>
2026-01-29 18:57:47 +0100

    Creado los archivos para crear app y los favicons para Sistemas operativos

--- app/index.php
+++ app/index.php
@@ -13,6 +13,19 @@ require_once 'db_config.php';
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
+    <!-- Manifest para Android -->
+    <link rel="manifest" href="manifest.json">
+
+    <!-- Meta tags para iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="Nombre App">
+    <link rel="apple-touch-icon" href="assets/icon-180.png">
+
+    <!-- Theme color -->
+    <meta name="theme-color" content="#000000">
+
+    
 </head>
 <body>
 
@@ -177,6 +190,11 @@ require_once 'db_config.php';
 <script src="js/chatbot.js"></script>
 <!-- <script src="js/jsNoticia.js"></script> -->
 <!-- <script src="js/modalNoticia.js"></script> -->
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+  }
+</script>
 
 
 </body>
